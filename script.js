728ca66b9522429b5fcfddb611e82741808b9aec
Luis Mendez <enriqmd08@gmail.com>
2023-09-18 15:55:50 -0400

    Muchos cambios hechos, los cambios lleguen hasta la agregacion de los productos y maquetados en JS

---
+++ script.js
@@ -0,0 +1,139 @@
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+
+const iconHamMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
+const iconCarMenu = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
+
+
+
+iconCarMenu.addEventListener('click', toggleAsideCar);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+iconHamMenu.addEventListener('click', toggleMobileMenu);
+
+
+
+
+function toggleDesktopMenu(){
+    // const isAsideMenuClosed = aside.classList.contains('inactive');
+
+    // if (!isAsideMenuClosed){
+    //     aside.classList.add('inactive');
+    // };
+    
+    desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+
+function toggleMobileMenu(){
+    // const isAsideMenuClosed = aside.classList.contains('inactive');
+
+    // if (!isAsideMenuClosed){
+    //     aside.classList.add('inactive');
+    // };
+
+    mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+}
+
+function toggleAsideCar(){
+    // const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    // if (!isMobileMenuClosed){
+    //     mobileMenu.classList.add('inactive');
+    // };
+
+    
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+}
+
+
+const productList = [];
+productList.push({
+    name: 'Bike',
+    precio: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+});
+
+productList.push({
+    name: 'Xbox Series S',
+    precio: 350,
+    image: 'https://assetsio.reedpopcdn.com/xbox-series-s-review-digitalfoundry-1605259161548.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp,'
+    
+});
+
+productList.push({
+    name: 'Iphone 15 pro max',
+    precio: 1000,
+    image: "https://imageio.forbes.com/specials-images/imageserve/64e74ad803781abed13b0612/Apple--iPhone--iPhone-15--iPhone-15-Pro--iPhone-15-Pro-Max--iPhone-15-release--new/0x0.jpg?format=jpg&crop=1275,956,x113,y0,safe&width=960",
+    
+});
+
+
+
+
+
+
+
+function renderProducts(arra){
+    for (Products of arra){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+    
+        const productImg = document.createElement('img');
+        productImg.classList.add('img-cover')
+        productImg.setAttribute('src', Products.image);
+    
+        /*
+        Estructura de product-info
+        */
+    
+        const producInfo = document.createElement('div');
+        producInfo.classList.add('product-info');
+    
+    
+        const productItemDiv = document.createElement('div');
+        productCard.classList.add('product-Item');
+        
+        const producPrecio = document.createElement('p');
+        producPrecio.innerText = '$' + Products.precio;
+        const productName = document.createElement('p');
+        productName.innerText = Products.name;
+    
+        
+    
+        /*
+        Estructura del figure
+       */
+        const productFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+    
+    /*
+    Por aca esta la estructura con appenchild, por contenedors padres e hijos.
+    */
+    
+        productFigure.appendChild(productImgCart);
+    
+        productItemDiv.append(producPrecio, productName)
+    
+        producInfo.append(productItemDiv, productFigure);
+    
+        productCard.append(productImg, producInfo);
+    
+        cardsContainer.appendChild(productCard);
+    
+    
+    }
+}
+
+renderProducts(productList);
+
